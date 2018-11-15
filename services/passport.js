d3fcaf5939e684da3b6f1853a0bdb6dae88eb147
Romane <romane71193@gmail.com>
2018-11-15 11:55:12 -0500

    sve user

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
 
-
+const User = mongoose.model('users')
 
 passport.use(
     new GoogleStrategy(
@@ -11,9 +12,17 @@ passport.use(
     clientSecret:keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log('access Token', accessToken);
-    console.log('refreshToken', refreshToken);
-    console.log('profile:', profile);
+
+    User.findOne({ googleID: profile.id})
+     .then((existingUser) => {
+         if(existingUser) {
+                done(null,existingUser);
+         } else {
+             new User({ googleID: profile.id }).save()
+             .then(user => done(null,user));
+         }
+     })
+
   }
  )
 );
