adbac88a6eac3dfaa465a3dbaebe2ce6349da931
Romane <romane71193@gmail.com>
2018-11-15 23:29:21 -0500

    add materialize

---
+++ client/src/components/Header.js
@@ -0,0 +1,19 @@
+import  React, {  Component } from 'react';
+
+    class Header extends Component {
+        render() {
+            return (
+                <nav>
+  <div className ="nav-wrapper">
+    <a className="left-brand-logo">Emaily</a>
+    <ul className="right hide-on-med-and-down">
+      <li><a>Log in with google</a></li>
+
+    </ul>
+  </div>
+</nav>
+            );
+        }
+    }
+
+    export default Header;
